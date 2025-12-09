c6d14f74fe7793b0254db013345000edac9c571b
Temuge Batpurev <temuge.batpurev@gmail.com>
2025-12-08 19:05:22 -0500

    training with post processed rotations

--- source/humanoid/humanoid/tasks/manager_based/humanoid/humanoid_env_cfg_walk.py
+++ source/humanoid/humanoid/tasks/manager_based/humanoid/humanoid_env_cfg_walk.py
@@ -27,7 +27,7 @@ joints = [
 
 usd_path = "/home/temuge/isaac_projects/humanoid_v1/robot_model/robot/humanoid.usd"
 #usd_path = '/home/temuge/isaac_projects/my_bots/humanoid_urdf/robot/humanoid.usd'
-animation_fps = 90
+animation_fps = 80
 num_frames = 135
 
 HUMANOID_CONFIG = ArticulationCfg(
@@ -272,7 +272,7 @@ class EventCfg:
 class RewardsCfg:
     """Reward terms for the MDP."""
 
-    progress = RewTerm(func=mdp.progress_reward, weight=0.5, params={"target_pos": (1000.0, 0.0, 0.0)})
+    progress = RewTerm(func=mdp.progress_reward, weight=1.0, params={"target_pos": (1000.0, 0.0, 0.0)})
     alive = RewTerm(func=mdp.is_alive, weight=10.0)
     torque_usage = RewTerm(func=mdp.joint_torques_l2, weight=-5e-4)
     joint_accel = RewTerm(func=mdp.joint_acc_l2, weight=-1e-6)
@@ -282,8 +282,8 @@ class RewardsCfg:
     move_to_target = RewTerm(func=mdp.move_to_target_bonus, weight=1.0, params={"threshold": 0.9, "target_pos": (1000.0, 0.0, 0.0)})
     feet_contact_tracking = RewTerm(func=mdp.feet_contact_tracking, weight = -0.5, params = {'animation_fps': animation_fps, 'num_frames': num_frames})
     xy_angular_vel = RewTerm(func=mdp.ang_vel_xy_l2, weight = -5e-1)
-    flat_orientation = RewTerm(func=mdp.flat_orientation_l2, weight = -1.0)
-    lateral_movement = RewTerm(func=mdp.lateral_motion, weight = -0.5)
+    flat_orientation = RewTerm(func=mdp.flat_orientation_l2, weight = -0.1)
+    lateral_movement = RewTerm(func=mdp.lateral_motion, weight = -0.1)
     #compound_reward = RewTerm(func=mdp.compound_reward, weight = 1.0, params = {'threshold': 5e-2})
     
     feet_slide = RewTerm(func=mdp.feet_slide, weight = -1.5, params={
